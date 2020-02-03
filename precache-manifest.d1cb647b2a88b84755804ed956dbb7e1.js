489ab8cdb8a4c5c7767c455bd81f913efe23b6a6
Adarsh-thedev <adarshpittu@gmail.com>
2020-02-03 23:13:10 +0530

    Updates

---
+++ precache-manifest.d1cb647b2a88b84755804ed956dbb7e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c8ef3123e908a278612666913f38f7",
+    "url": "/restaurant-project-2/index.html"
+  },
+  {
+    "revision": "084c2b78c72fa0564a32",
+    "url": "/restaurant-project-2/static/css/2.0b5e3341.chunk.css"
+  },
+  {
+    "revision": "8e83c258565ce7f7c1e9",
+    "url": "/restaurant-project-2/static/css/main.de3815aa.chunk.css"
+  },
+  {
+    "revision": "084c2b78c72fa0564a32",
+    "url": "/restaurant-project-2/static/js/2.c405af05.chunk.js"
+  },
+  {
+    "revision": "dd0ec1d6c231f3beab44fa52966a52db",
+    "url": "/restaurant-project-2/static/js/2.c405af05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e83c258565ce7f7c1e9",
+    "url": "/restaurant-project-2/static/js/main.ad3c3531.chunk.js"
+  },
+  {
+    "revision": "3b06779f76023a434f10",
+    "url": "/restaurant-project-2/static/js/runtime-main.80256e05.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurant-project-2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurant-project-2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurant-project-2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurant-project-2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurant-project-2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
